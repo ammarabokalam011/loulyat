16d0bd7ac0eafe8ce77fda9b4c429302f6ea6988
ammar011 <ammarabokalam0@gmail.com>
2022-03-15 05:39:38 +0330

    add api and auth

---
+++ app/Http/Controllers/API/UserAPIController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Requests\API\CreateUserAPIRequest;
+use App\Http\Requests\API\UpdateUserAPIRequest;
+use App\Models\User;
+use App\Repositories\UserRepository;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AppBaseController;
+use Response;
+
+/**
+ * Class UserController
+ * @package App\Http\Controllers\API
+ */
+
+class UserAPIController extends AppBaseController
+{
+    /** @var  UserRepository */
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepo)
+    {
+        $this->userRepository = $userRepo;
+    }
+
+    /**
+     * Display a listing of the User.
+     * GET|HEAD /users
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $users = $this->userRepository->all(
+            $request->except(['skip', 'limit']),
+            $request->get('skip'),
+            $request->get('limit')
+        );
+
+        return $this->sendResponse($users->toArray(), 'Users retrieved successfully');
+    }
+
+
+    /**
+     * Display the specified User.
+     * GET|HEAD /users/{id}
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function show($id)
+    {
+        /** @var User $user */
+        $user = $this->userRepository->find($id);
+
+        if (empty($user)) {
+            return $this->sendError('User not found');
+        }
+
+        return $this->sendResponse($user->toArray(), 'User retrieved successfully');
+    }
+
+
+}
